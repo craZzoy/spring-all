42e51822b675d20dea6a48174ef065b9d6c2612c
jackma <1510134048@qq.com>
2020-07-13 08:24:40 +0800

    zuul

--- springcloud-demo/ch7-2-zuul-server/src/main/java/com/demo/ZuulServerStarter.java
+++ springcloud-demo/ch7-2-zuul-server/src/main/java/com/demo/ZuulServerStarter.java
@@ -1,5 +1,6 @@
 package com.demo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -8,4 +9,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableDiscoveryClient
 @EnableZuulProxy
 public class ZuulServerStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerStarter.class, args);
+    }
 }
